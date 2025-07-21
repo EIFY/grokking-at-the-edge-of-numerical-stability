e404ccb68df7d5f3612f538f31f8b2297ee28318
EIFY <chuanchih@gmail.com>
2025-07-21 14:20:51 -0700

    GELU exp., filter out all settings that don't grok reliably

--- lipschitz_experiments.sh
+++ lipschitz_experiments.sh
@@ -5,15 +5,18 @@ DEVICE="cuda:0"
 AR="--all_reg"
 EM="--use_embedding"
 
-for op in add_mod product_mod
+for AF in "--activation_function GELU" ""
 do
-    for i in {0..63}
+    for op in add_mod product_mod
     do
-        for LR in 0.001 0.002 0.005 0.01 0.02 0.05 0.1
+        for i in {0..63}
         do
-            for WD in 2. 4. 6. 8. 10.
+            for LR in 0.001 0.002 0.005 0.01 0.02 0.05 0.1
             do
-                python grokking_experiments.py --binary_operation $op --lr $LR --weight_decay $WD --num_epochs 1001 --log_frequency 1 --device $DEVICE --train_fraction 0.4 --train_dtype bfloat16 --cross_entropy_dtype bfloat16 --seed $i $EM $AR
+                for WD in 2. 4. 6. 8. 10.
+                do
+                    python grokking_experiments.py --binary_operation $op --lr $LR --weight_decay $WD --num_epochs 1001 --log_frequency 1 --device $DEVICE --train_fraction 0.4 --train_dtype bfloat16 --cross_entropy_dtype bfloat16 --seed $i $EM $AR $AF
+                done
             done
         done
     done
